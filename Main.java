353fdc8b5e41856818add13f25a88c4f7a15ba5f
vaishnavig1706 <vaishnavigurav0603@gmail.com>
2025-11-08 20:15:11 +0530

    second commit

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("output from java program");
-        System.out.println("Hello")
+        System.out.println("Hello");
     }
 }
