9a35c181dc612f03cf01b0b069a05deaf7b4486c
vaishnavig1706 <vaishnavigurav0603@gmail.com>
2025-11-08 20:10:18 +0530

    add commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("output from java program");
+        System.out.println("Hello")
     }
 }
